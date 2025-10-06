307fe9643b55aa0844d9dac83007a42ebb421fa6
Victor Gallego <vidagum123@hotmail.com>
2025-10-06 10:05:19 -0500

    improvement on rating to checkif the user already commented and properly shows the comments on rating

--- src/app/lesson-module/rating/rating.component.ts
+++ src/app/lesson-module/rating/rating.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
 import { LessonService } from 'src/app/services/lesson.service';
 
@@ -15,7 +15,7 @@ import { LESSON_STATES } from 'src/app/services/DATA';
     styleUrls: ['./rating.component.css'],
     standalone: false
 })
-export class RatingComponent implements OnInit {
+export class RatingComponent implements OnInit, OnDestroy {
     public title: string;
     public identity;
     public token;
@@ -29,6 +29,7 @@ export class RatingComponent implements OnInit {
     public comment;
 
     public ratingForm;
+    private formSubscription: any;
 
     public errorMsg;
     public successMsg;
@@ -65,7 +66,20 @@ export class RatingComponent implements OnInit {
 
 
     ngOnInit(): void {
+        // Suscribir una sola vez a los cambios del formulario
+        this.formSubscription = this.ratingForm.valueChanges.subscribe(val => {
+            if (val) {
+                this.status = null;
+                this.submitted = false;
+                this.userAlreadyCommented = null;
+            }
+        });
+    }
 
+    ngOnDestroy(): void {
+        if (this.formSubscription) {
+            this.formSubscription.unsubscribe();
+        }
     }
 
 
@@ -79,9 +93,10 @@ export class RatingComponent implements OnInit {
     async onSubmit() {   
         this.restartValues();    
 
-        this.userAlreadyCommented = this.lesson.comments.find(
-            comment => this.identity._id == comment.user._id
-            );
+        // Validar si el usuario ya comentó (lista poblada)
+        this.userAlreadyCommented = Array.isArray(this.lesson?.comments) ? this.lesson.comments.find(
+            (comment) => comment && comment.user && (this.identity._id == comment.user._id)
+        ) : null;
 
         this.submitted = true;
 
@@ -94,7 +109,7 @@ export class RatingComponent implements OnInit {
             this.identity._id
         );
 
-        this.comment.score = this.ratingForm.value.rating;
+        this.comment.score = Number(this.ratingForm.value.rating);
 
 
         let response = await this._commentService.addComment(this.token, this.comment)
@@ -107,20 +122,25 @@ export class RatingComponent implements OnInit {
 
         if (response && response.comment._id) {            
 
-            if(this.lesson.score == 0){             
-                this.lesson.score += response.comment.score;
-            }else{
-                this.lesson.score += response.comment.score;
-                this.lesson.score /= 2;                
-            }
+            // Calcular promedio correctamente en base al número de comentarios actuales
+            const totalRatingsBefore = Array.isArray(this.lesson.comments) ? this.lesson.comments.length : 0;
+            const currentTotal = (Number(this.lesson.score) || 0) * totalRatingsBefore;
+            const newAverage = (currentTotal + Number(response.comment.score)) / (totalRatingsBefore + 1);
+            this.lesson.score = newAverage;
 
+            // Agregar el nuevo comentario por ID a la lección
+            if (!Array.isArray(this.lesson.comments)) {
+                this.lesson.comments = [];
+            }
             this.lesson.comments.push(response.comment._id);
 
             this._lessonService.editLesson(this.token, this.lesson).subscribe(
                 response => {
-                    if (response && response.lesson._id) {                        
+                    if (response && response.lesson && response.lesson._id) {                        
                         this.status = "success";
+                        // La API devuelve la lección con comentarios poblados
                         this.lesson = response.lesson;
+                        this.ratingForm.reset();
                     }
                 },
                 error => {
