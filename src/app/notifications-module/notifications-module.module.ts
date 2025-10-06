85c91c9e11f6bf4b00fbf52c751230ebaae895c0
Victor Gallego <vidagum123@hotmail.com>
2025-10-06 10:08:48 -0500

    improvement on noficiation module

--- src/app/notifications-module/notifications-module.module.ts
+++ src/app/notifications-module/notifications-module.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { NotificationsComponent } from './notifications/notifications.component';
 
 @NgModule({
-  declarations: [NotificationsComponent],
   imports: [
-    CommonModule,
     RouterModule.forChild([
       { path: '', component: NotificationsComponent }
     ])
-  ],
-  exports: [NotificationsComponent]
+  ]
 })
 export class NotificationsModule { }
