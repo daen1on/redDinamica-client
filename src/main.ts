d95603110de76a7b7b3d135ed44076805688d0d9
Victor Gallego <vidagum123@hotmail.com>
2025-10-10 22:44:01 -0500

    moment properly handles dates in spanish now

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,23 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import 'moment/locale/es';
+import * as moment from 'moment';
 
 if (environment.production) {
   enableProdMode();
+}
+
+// Establecer el locale global de Moment a español
+moment.locale('es');
+
+// Silenciar logs según configuración de entorno
+if (!environment.enableConsoleLogs) {
+  const noop = () => {};
+  console.log = noop;
+  console.info = noop;
+  console.debug = noop;
+  // Deja warn y error activos para diagnósticos críticos
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
